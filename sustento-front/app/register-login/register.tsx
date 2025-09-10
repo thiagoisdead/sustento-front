14b060cfaceab90b878e31136c781542ef5d6a00
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-09-09 23:34:55 -0300

    formulário de registro e login, com verificações de email e senha, além de inputs customizados

---
+++ sustento-front/app/register-login/register.tsx
@@ -0,0 +1,139 @@
+import { Link, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import Healthy from "../../assets/breakfast.svg"
+import { useFonts, EpundaSlab_400Regular } from "@expo-google-fonts/epunda-slab";
+import { Button, TextInput } from 'react-native-paper';
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Dados } from '../../types/type';
+export default function Register() {
+
+  const router = useRouter()
+  const handleChange = () => {
+    if (!dados) return
+    if (dados.password !== dados?.confirmPassword) return
+  }
+
+
+  const [dados, setDados] = useState<Dados>({
+    name: "",
+    password: "",
+    email: "",
+    confirmPassword: "",
+  });
+  const [passwordSee, setPasswordSee] = useState<Boolean>();
+  const [passwordConfirmationSee, setPasswordConfirmationSee] = useState<Boolean>();
+
+
+
+  const textInputStyle = {
+    backgroundColor: "#f8fafc",
+    borderRadius: 10,
+    elevation: 10,
+  };
+  const textInputTheme = {
+    roundness: 10,
+    colors: {
+      primary: "rgba(87, 143, 26, 255)",
+      outline: "transparent",
+      background: "transparent",
+      onSurfaceVariant: "#0d0c22",
+    },
+  };
+
+
+  const [fontsLoaded] = useFonts({
+    EpundaSlab_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <KeyboardAwareScrollView style={{ flex: 1 }} contentContainerStyle={{ flexGrow: 1 }}
+      enableOnAndroid={true}
+      extraScrollHeight={20}>
+      <View style={styles.container}>
+        <View style={{ height: '10%', width: '100%', paddingHorizontal: 10, display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+          <Button
+            icon="arrow-left"
+            mode="contained"
+            onPress={() => router.push('/register-login/')}
+          >
+            Voltar
+          </Button>
+        </View>
+        <View style={{ height: '10%', marginTop: 40 }}>
+          <Text style={{ fontFamily: "EpundaSlab_400Regular", fontSize: 40 }}>Sustento</Text>
+        </View>
+        <View style={{ width: '100%', flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+          <View style={{ width: '100%', gap: 10, padding: 20 }}>
+            <TextInput
+              label="Nome"
+              mode="outlined"
+              value={dados?.name || ""}
+              underlineColor="transparent"
+              onChangeText={(text) => setDados({ ...dados, name: text })}
+              style={textInputStyle}
+              theme={textInputTheme}
+            />
+            <TextInput
+              label="Email"
+              mode="outlined"
+              value={dados?.email || ""}
+              underlineColor="transparent"
+              onChangeText={(text) => setDados({ ...dados, email: text })}
+              style={textInputStyle}
+              theme={textInputTheme}
+            />
+            <TextInput
+              label="Senha"
+              mode="outlined"
+              value={dados?.password || ""}
+              underlineColor="transparent"
+              onChangeText={(text) => setDados({ ...dados, password: text })}
+              style={textInputStyle}
+              theme={textInputTheme}
+              secureTextEntry={!passwordSee}
+              right={
+                <TextInput.Icon forceTextInputFocus={false} icon={passwordSee ? 'eye-off' : 'eye'} onPress={() => setPasswordSee(!passwordSee)} />
+              }
+            />
+            <TextInput
+              label="Confirme sua senha"
+              mode="outlined"
+              value={dados?.confirmPassword || ""}
+              underlineColor="transparent"
+              onChangeText={(text) => setDados({ ...dados, confirmPassword: text })}
+              style={textInputStyle}
+              theme={textInputTheme}
+              secureTextEntry={!passwordConfirmationSee}
+              right={
+                <TextInput.Icon forceTextInputFocus={false} icon={passwordConfirmationSee ? 'eye-off' : 'eye'} onPress={() => setPasswordConfirmationSee(!passwordConfirmationSee)} />
+              }
+            />
+          </View>
+        </View>
+        <View style={{ width: '100%', height: '15%', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center' }}>
+          <Button mode='contained' style={{ width: 200 }}>
+            Cadastrar
+          </Button>
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    width: '100%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    // backgroundColor: 'blue',
+    paddingVertical: 100
+
+  },
+});
