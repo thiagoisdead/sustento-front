13214cea2cf761a44d3aa0583bd31cc6e3fc516a
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-09-24 00:07:05 -0300

    🚧 just realocating space and placing navbar on screen

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,11 +1,14 @@
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
 
 export default function Home() {
-
+  const router = useRouter()
   return (
     <View style={styles.container}>
+      <Button onPress={() => router.push('auth/login')}>eqweq</Button>
       <Text>!!!</Text>
     </View>
   );
@@ -14,7 +17,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ece1c3',
     alignItems: 'center',
     justifyContent: 'center',
   },
