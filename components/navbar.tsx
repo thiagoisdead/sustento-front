5a5884293c076fd2e39d25440437fa6dd5f3f8b1
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-09-24 00:07:25 -0300

    ✨ navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,10 +3,12 @@ import Entypo from '@expo/vector-icons/Entypo';
 import Feather from '@expo/vector-icons/Feather';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { useRouter } from 'expo-router';
+import { Button } from 'react-native-paper';
 
 
 export default function NavBar() {
-
+  const router = useRouter()
   return (
     <View style={styles.container}>
       <View style={styles.child}><Entypo name="calendar" size={35} color="black" />
@@ -18,10 +20,10 @@ export default function NavBar() {
       <View style={styles.child}><MaterialCommunityIcons name="food-apple-outline" size={35} color="black" />
         {/* <Text>Dieta</Text> */}
       </View>
-      <View style={styles.child}><MaterialCommunityIcons name="food-turkey" size={35} color="black" />
+      <View style={styles.child}><MaterialCommunityIcons name="food-turkey" size={35} color="black"  />
         {/* <Text>Alimentos</Text> */}
       </View>
-      <View style={styles.child}><Ionicons name="person" size={35} color="black" />
+      <View style={styles.child}><Ionicons name="person" size={35} color="black" onPress={() => { router.push('/profile/seeProfile') }} />
         {/* <Text>Perfil</Text> */}
       </View>
     </View>
@@ -30,10 +32,11 @@ export default function NavBar() {
 
 const styles = StyleSheet.create({
   container: {
-    height: '12%',
     width: '100%',
     backgroundColor: '#1a2323',
     alignItems: 'center',
+    padding: 2,
+
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
@@ -42,6 +45,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 5,
+    padding: 8,
+    borderColor: '#578f1a',
+    borderWidth: 2
   },
 });
