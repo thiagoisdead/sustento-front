9e9f465e321784de3a9f0483f5bd94089d229c54
Thiago Navarro <dollynhoextremeyt@gmail.com>
2025-10-02 23:50:56 -0300

    ✨ add MealsHome component with basic layout

---
+++ app/meals/mealsHome.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from "react-native"
+
+export default function MealsHome() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ece1c3'
+  }
+})
